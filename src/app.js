fab9ec01e73f47d8c91076abec0b449b30d90d39
Namra Vekariya <vekariyanamra502@gmail.com>
2024-12-26 22:37:16 +0530

    user routes and control

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use(cookieParser());
 //routes
 import userRouter from "./routes/user.routes.js";
 
-app.use("api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 export { app };
