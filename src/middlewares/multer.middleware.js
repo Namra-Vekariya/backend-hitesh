c95b15eb39de5c93e1ff3765533c654d81f4a847
Namra Vekariya <vekariyanamra502@gmail.com>
2025-01-02 23:17:50 +0530

    add user account management

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,14 @@
 import multer from "multer";
-import path from "path";
+
 const storage = multer.diskStorage({
-  //cb=callback
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
-    // cb(null, path.join(__dirname, "..", "public", "temp"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+    //cb=callback
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp");
+        // cb(null, path.join(__dirname, "..", "public", "temp"));
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
 });
 
 export const upload = multer({ storage });
